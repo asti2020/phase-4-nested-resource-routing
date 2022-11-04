c488b42acad8d548ee0619cad2cbc477097e7805
Astika Luintel <astild2020@gmail.com>
2022-11-03 21:55:46 -0400

    commit tree cd0b06193f24c962741e150487ad01e16714a57e

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -6,6 +6,17 @@ class DogHousesController < ApplicationController
     render json: dog_house
   end
 
+  def reviews_index
+    dogs = DogHouse.find(params[:dog_house_id])
+    review = dogs.reviews
+    render json: review, include: :dog_house, status: :ok
+  end
+
+  def review
+    reviews = Review.find(params[:id])
+    render json: reviews, include: :dog_house, status: :ok
+  end
+
   private
 
   def render_not_found_response
